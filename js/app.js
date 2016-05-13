5399d8ca13742530ea77faa2c4019e1546dc935f
Juan Marcelo <jarteaga@agetic.gob.bo>
2016-05-13 11:01:14 -0400

    Estable funcionando con django pero falta pruebas con angular material

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@
   var app = angular.module('djangoRestFormlyExample', ['formly', 'formlyBootstrap']);
 
 
-  app.controller('MainCtrl', function MainCtrl(formlyVersion) {
+  app.controller('MainCtrl', function MainCtrl(formlyVersion, $http) {
     var vm = this;
     // function assignment
     vm.onSubmit = onSubmit;
@@ -26,37 +26,52 @@
       formlyVersion: formlyVersion
     };
 
-    vm.model = {
-      note: "",
-      score: 0
-    };
+    // vm.model = {
+    //   note: "",
+    //   score: 0
+    // };
     vm.options = {
       formState: {
         awesomeIsForced: false
       }
     };
-
-    vm.fields = DjangoRestFormly.DjangoRestFrameworkAdapter({
-      "first_name": {
-          "type": "string",
-          label: "First Name",
-          "required": true,
-          "read_only": false,
-          max_length: 10
-      },
-      "last_name": {
-          "type": "string",
-          "required": true,
-          "read_only": false,
-          "label": "Family Name (Max: 10)",
-          "max_length": 10
-      },
-      "age": {
-          "type": "integer",
-          "required": true,
-          "read_only": false
-      }
+    var urlSearch = "http://127.0.0.1:8000/api/designacion_horarios/1/";
+    // var urlSearch = "http://127.0.0.1:8000/api/designacion_horarios/";
+    $http({method: 'OPTIONS', url:urlSearch}).success(function(data){
+      vm.fields = DjangoRestFormly.toFormly(data.actions.PUT);
+      /*$http({method: 'GET', url:urlSearch}).success(function(data){
+        vm.model = data;
+      });*/
+      vm.model = {
+        id: 1,
+        fecha_ini: new Date("2016-05-03"),
+        fecha_fin: new Date("2016-05-03"),
+        usuario: "4",
+        horario: "1"
+      };
     });
+
+    // vm.fields = DjangoRestFormly.toFormly({
+    //   "first_name": {
+    //       "type": "string",
+    //       label: "First Name",
+    //       "required": true,
+    //       "read_only": false,
+    //       max_length: 10
+    //   },
+    //   "last_name": {
+    //       "type": "string",
+    //       "required": true,
+    //       "read_only": false,
+    //       "label": "Family Name (Max: 10)",
+    //       "max_length": 10
+    //   },
+    //   "age": {
+    //       "type": "integer",
+    //       "required": true,
+    //       "read_only": false
+    //   }
+    // });
 
     // function definition
     function onSubmit() {
